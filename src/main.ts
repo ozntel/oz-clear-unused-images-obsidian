d49f622505b3018a72335129ebdbef27499aec2f
Ozan Tellioglu <ozntel@gmail.com>
2021-05-10 20:36:41 +0200

    Async function & Spacing

--- src/main.ts
+++ src/main.ts
@@ -21,43 +21,43 @@ export default class OzanClearImages extends Plugin {
 	// Compare Used Images with all images and return unused ones
 	clearUnusedImages = async () => {
 		var all_images_in_vault: TFile[] = this.getAllImagesInVault();
-		var unused_images : TFile[] = [];
+		var unused_images: TFile[] = [];
 		var used_images_set: Set<string>;
 
 		// Get Used Images in All Markdown Files
 		used_images_set = this.getImagePathSetForVault();
 
 		// Compare All Images vs Used Images
-		all_images_in_vault.forEach( img => {
-			if(!used_images_set.has(img.path)) unused_images.push(img)
+		all_images_in_vault.forEach(img => {
+			if (!used_images_set.has(img.path)) unused_images.push(img)
 		});
 
 		var len = unused_images.length;
-		
-		if(len > 0){
+
+		if (len > 0) {
 			console.log('[+] Deleting ' + len + ' images.');
 			this.deleteFilesInTheList(unused_images);
 			new Notice(len + ' image(s) in total deleted.');
 			console.log('[+] Delete completed.');
-		}else{
+		} else {
 			new Notice('All images are used. Nothing was deleted.');
 		}
 	}
 
 	// Clear Images From the Provided List
-	deleteFilesInTheList = (fileList: TFile[]) => {
-		fileList.forEach( async (file) => {
+	deleteFilesInTheList = async (fileList: TFile[]) => {
+		for (let file of fileList) {
 			await this.app.vault.delete(file);
 			console.log('Deleted: ' + file.path);
-		})
+		}
 	}
 
 	// Getting all available images saved in vault
 	getAllImagesInVault = (): TFile[] => {
-		let allFiles : TFile[] = this.app.vault.getFiles();
-		let images : TFile[] = [];
-		for(let i=0; i < allFiles.length; i++){
-			if(this.imageExtensions.has(allFiles[i].extension)){
+		let allFiles: TFile[] = this.app.vault.getFiles();
+		let images: TFile[] = [];
+		for (let i = 0; i < allFiles.length; i++) {
+			if (this.imageExtensions.has(allFiles[i].extension)) {
 				images.push(allFiles[i]);
 			}
 		}
@@ -68,11 +68,11 @@ export default class OzanClearImages extends Plugin {
 	getImagePathSetForVault = (): Set<string> => {
 		var images_set: Set<string> = new Set();
 		var resolvedLinks = this.app.metadataCache.resolvedLinks;
-		if(resolvedLinks){
-			for(const [md_file, links] of Object.entries(resolvedLinks)){
-				for(const [file_path, nr] of Object.entries(resolvedLinks[md_file])){
+		if (resolvedLinks) {
+			for (const [md_file, links] of Object.entries(resolvedLinks)) {
+				for (const [file_path, nr] of Object.entries(resolvedLinks[md_file])) {
 					var image_match = file_path.match(this.imageRegex);
-					if(image_match) images_set.add(image_match[0]);
+					if (image_match) images_set.add(image_match[0]);
 				}
 			}
 		}
