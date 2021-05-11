f0e5336cb2e8d539d11ae21167ce02702992adbf
Ozan Tellioglu <ozntel@gmail.com>
2021-05-11 12:55:28 +0200

    Initial Version of Auto Clear

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,46 @@
-import { Plugin, TFile, Notice } from 'obsidian';
+import { Plugin, TFile, Notice, PluginSettingTab, Setting, App } from 'obsidian';
+
+interface OzanClearImagesSettings {
+	autoClearInterval: number;
+	autoClearOnBoot: boolean;
+}
+
+const DEFAULT_SETTINGS: OzanClearImagesSettings = {
+	autoClearInterval: 0,
+	autoClearOnBoot: false,
+}
 
 export default class OzanClearImages extends Plugin {
 
-	onload() {
+	settings: OzanClearImagesSettings;
+	timeoutID: number;
+
+	async onload() {
 		console.log("Loading oz-clear-unused-images plugin")
+		this.addSettingTab(new OzanClearImagesSettingsTab(this.app, this));
+		await this.loadSettings();
 		this.addCommand({
 			id: 'clear-images-obsidian',
 			name: 'Clear Unused Images in Vault',
 			callback: () => this.clearUnusedImages()
 		});
+		this.app.workspace.on('layout-ready', () => {
+			if (this.settings.autoClearOnBoot) {
+				setTimeout(() => this.clearUnusedImages(), 3000)
+			}
+		})
 	}
 
 	onunload() {
 		console.log('Unloading oz-clear-unused-images plugin');
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
 	}
 
 	imageRegex = /.*(jpe?g|png|gif|svg|bmp)/;
@@ -79,5 +107,72 @@ export default class OzanClearImages extends Plugin {
 		return images_set
 	}
 
+	// Auto Clear Images
+	startAutoClean(minutes?: number) {
+		this.timeoutID = window.setTimeout(() => {
+			this.clearUnusedImages();
+			this.startAutoClean();
+		}, (minutes ?? this.settings.autoClearInterval) * 60000)
+	}
+
+	// Clear Timeout
+	clearAutoClean(): boolean {
+		if (this.timeoutID) {
+			window.clearTimeout(this.timeoutID);
+			return true;
+		}
+		return false;
+	}
+
 }
 
+class OzanClearImagesSettingsTab extends PluginSettingTab {
+
+	plugin: OzanClearImages;
+
+	constructor(app: App, plugin: OzanClearImages) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		let { containerEl } = this;
+
+		containerEl.empty();
+
+		containerEl.createEl("h2", { text: "Clear Images Settings" });
+
+		new Setting(containerEl)
+			.setName('Clear image interval (minutes)')
+			.setDesc('Check and clear the unused images every X monutes. To disable auto clear, give zero (default)')
+			.addText((text) => text
+				.setValue(String(this.plugin.settings.autoClearInterval))
+				.onChange((value) => {
+					if (!isNaN(Number(value))) {
+						this.plugin.settings.autoClearInterval = Number(value);
+						this.plugin.saveSettings();
+						if (this.plugin.settings.autoClearInterval > 0) {
+							this.plugin.clearAutoClean();
+							this.plugin.startAutoClean(this.plugin.settings.autoClearInterval);
+							new Notice(`Auto clear enabled! Every ${this.plugin.settings.autoClearInterval} minutes`)
+						} else if (this.plugin.settings.autoClearInterval <= 0) {
+							this.plugin.clearAutoClean() && new Notice('Auto clear disabled')
+						}
+					} else {
+						new Notice("Please specify a valid number.")
+					}
+				})
+			)
+
+		new Setting(containerEl)
+			.setName("Clear Images on startup")
+			.setDesc("Automatically clear images when Obsidian starts")
+			.addToggle((toggle) => toggle
+				.setValue(this.plugin.settings.autoClearOnBoot)
+				.onChange((value) => {
+					this.plugin.settings.autoClearOnBoot = value;
+					this.plugin.saveSettings();
+				})
+			);
+	}
+}
